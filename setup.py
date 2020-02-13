5e913bead2f0ff1cf956d96bb47270660fa8c8f1
Hagen Wierstorf <hwierstorf@audeering.com>
2020-02-13 16:44:56 +0100

    Switch to stable in setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description="\n".join(readme[2:]),
     zip_safe=False,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
