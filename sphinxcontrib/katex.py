035e828cba71b81fd3c3d7a41b59e6d44274b35e
Hagen Wierstorf <hwierstorf@audeering.com>
2020-02-13 16:48:33 +0100

    Switch to KaTeX v0.11.1

--- sphinxcontrib/katex.py
+++ sphinxcontrib/katex.py
@@ -24,7 +24,7 @@ from sphinx.util.osutil import copyfile
 
 
 __version__ = '0.5.1'
-katex_version = '0.10.2'
+katex_version = '0.11.1'
 filename_css = 'katex-math.css'
 filename_autorenderer = 'katex_autorenderer.js'
 
