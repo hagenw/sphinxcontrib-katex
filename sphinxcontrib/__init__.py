62ffcca9d7af90674b945729724a447949dda9b2
Hagen Wierstorf <hwierstorf@audeering.com>
2021-05-17 09:03:43 +0200

    Use setup.cfg

--- sphinxcontrib/__init__.py
+++ sphinxcontrib/__init__.py
@@ -9,4 +9,15 @@
     See https://bitbucket.org/birkenfeld/sphinx-contrib for other extensions.
 """
 
-__import__('pkg_resources').declare_namespace(__name__)
+__all__ = []
+
+
+# Dynamically get the version of the installed module
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+    __version__ = pkg_resources.get_distribution(__name__).version
+except Exception:  # pragma: no cover
+    pkg_resources = None  # pragma: no cover
+finally:
+    del pkg_resources
