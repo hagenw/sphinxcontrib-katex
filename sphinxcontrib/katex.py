92e3bb1440f23712c0693b158d6bc5a94496fbe7
Shiv Shankar Dayal <shivshankar.dayal@gmail.com>
2022-05-21 16:08:13 +0530

    new implementation for server side rendering to sphinx katex plugin

--- sphinxcontrib/katex.py
+++ sphinxcontrib/katex.py
@@ -10,6 +10,8 @@
     :license: MIT, see LICENSE for details.
 """
 
+import atexit
+import json
 import os
 import platform
 import re
@@ -18,6 +20,14 @@ from docutils import nodes
 from tempfile import mkdtemp
 from textwrap import dedent
 import subprocess
+import socket
+import struct
+import tempfile
+import time
+
+from contextlib import closing, contextmanager
+from pathlib import Path
+from subprocess import PIPE, Popen, TimeoutExpired
 
 from sphinx.locale import _
 from sphinx.errors import ExtensionError
@@ -28,6 +38,35 @@ __version__ = '0.8.6'
 katex_version = '0.15.2'
 filename_css = 'katex-math.css'
 filename_autorenderer = 'katex_autorenderer.js'
+SRC_DIR = Path(__file__).parent
+SCRIPT_PATH = str(SRC_DIR / "katex-server.js")
+
+ONE_MILLISECOND = 0.001
+
+TIMEOUT_EXPIRED_TEMPLATE = (
+    "Rendering {} is taking too long. Try increasing RENDER_TIMEOUT"
+)
+
+STARTUP_TIMEOUT_EXPIRED = (
+    "KaTeX server did not came up after {} seconds. "
+    "Try increasing STARTUP_TIMEOUT."
+)
+
+KATEX_DEFAULT_OPTIONS = {
+    # Prefer KaTeX's debug coloring by default. This will not raise exceptions.
+    "throwOnError": False
+}
+
+KATEX_PATH = None
+
+# How long to wait for the render server to start in seconds
+STARTUP_TIMEOUT = 5.0
+
+# Timeout per rendering request in seconds
+RENDER_TIMEOUT = 5.0
+
+# nodejs binary to run javascript
+NODEJS_BINARY = "node"
 
 
 def latex_defs_to_katex_macros(defs):
@@ -74,25 +113,6 @@ def get_latex(node):
         return node['latex']  # for Sphinx < 1.8.0
     else:
         return node.astext()  # for Sphinx >= 1.8.0
-
-
-def run_katex(latex, *options):
-    if platform.system() == 'Windows':
-        cmd = 'katex.cmd'
-    else:
-        cmd = 'katex'
-    p = subprocess.Popen(
-        (cmd, ) + options,
-        stdout=subprocess.PIPE,
-        stdin=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        env=os.environ.copy()
-    )
-    stdout, stderr = p.communicate(latex.encode('utf-8'))
-    if p.returncode:
-        msg = 'KaTeX failed with\n: ' + stderr.decode('utf-8')
-        raise RuntimeError(msg)
-    return stdout.decode('utf-8').rstrip('\r\n')
 
 
 def html_visit_math(self, node):
@@ -292,78 +312,6 @@ def get_node_equation_number(writer, node):
 
     return number
 
-import atexit
-import json
-import os
-import shutil
-import socket
-import struct
-import tempfile
-import time
-from contextlib import closing, contextmanager
-from pathlib import Path
-from subprocess import PIPE, Popen, TimeoutExpired
-
-SRC_DIR = Path(__file__).parent
-SCRIPT_PATH = str(SRC_DIR / "render-katex.js")
-
-ONE_MILLISECOND = 0.001
-
-TIMEOUT_EXPIRED_TEMPLATE = (
-    "Rendering {} took too long. Consider increasing KATEX_RENDER_TIMEOUT"
-)
-
-STARTUP_TIMEOUT_EXPIRED_TEMPLATE = (
-    "KaTeX server did not start up after {} seconds. "
-    "Consider increasing KATEX_STARTUP_TIMEOUT."
-)
-
-# Default KaTeX options.
-KATEX_DEFAULT_OPTIONS = {
-    # Prefer KaTeX's debug coloring by default
-    "throwOnError": False
-}
-
-# Path to the KaTeX program to run
-KATEX_PATH = None
-
-# How long to wait for the render server to start in seconds
-KATEX_STARTUP_TIMEOUT = 1.0
-
-# Timeout per rendering request in seconds
-KATEX_RENDER_TIMEOUT = 1.0
-
-# nodejs binary to run javascript
-KATEX_NODEJS_BINARY = "node"
-
-# Preamble to prepend to any rendered LaTeX
-KATEX_PREAMBLE = None
-
-# A list of file-local additions to the preamble
-LOCAL_PREAMBLES = []
-
-
-def push_preamble(preamble):
-    LOCAL_PREAMBLES.append(preamble)
-
-
-def reset_preamble():
-    LOCAL_PREAMBLES.clear()
-
-
-def get_preamble():
-    preamble = KATEX_PREAMBLE
-
-    if len(LOCAL_PREAMBLES) > 0:
-        local_preamble = "\n".join(LOCAL_PREAMBLES)
-        if preamble is None:
-            preamble = local_preamble
-        else:
-            preamble += "\n" + local_preamble
-
-    return preamble
-
-
 @contextmanager
 def socket_timeout(sock, timeout):
     """Set the timeout on a socket for a context and restore it afterwards"""
@@ -377,43 +325,43 @@ def socket_timeout(sock, timeout):
         sock.settimeout(original)
 
 
-def free_port():
+def random_free_port():
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
-        # Reuse sockets without waiting for them to expire
+        # reuse sockets
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        # Choosing port=0 makes the OS choose a random available port
+        # choose port=0 for random port
         sock.bind(("127.0.0.1", 0))
 
-        # Report the port we were assigned
-        _, port = sock.getsockname()
-        return port
+        # return port
+        _, p = sock.getsockname()
+        return p
 
 
 class KaTeXError(Exception):
     pass
 
 
-class RenderServer:
+class KaTeXServer:
     """Manages and communicates with an instance of the render server"""
 
-    # The length of a message is transmitted as 32-bit little-endian integer
+    # Message length is 32-bit little-endian integer
     LENGTH_STRUCT = struct.Struct("<i")
 
-    # A global instance
-    RENDER_SERVER = None
+    # global instance
+    KATEX_SERVER = None
 
-    # How long to wait for the server to stop in seconds
+    # wait for the server to stop in seconds
     STOP_TIMEOUT = 0.1
 
     @staticmethod
     def timeout_error(self, timeout):
-        message = STARTUP_TIMEOUT_EXPIRED_TEMPLATE.format(timeout)
+        message = STARTUP_TIMEOUT_EXPIRED.format(timeout)
         return KaTeXError(message)
 
     @staticmethod
     def build_command(socket=None, port=None):
-        cmd = [KATEX_NODEJS_BINARY, SCRIPT_PATH]
+        cmd = [NODEJS_BINARY, SCRIPT_PATH]
 
         if socket is not None:
             cmd.extend(["--socket", str(socket)])
@@ -427,15 +375,14 @@ class RenderServer:
         return cmd
 
     @classmethod
-    def start_unix_socket(cls, rundir, timeout):
+    def start_server_process(cls, rundir, timeout):
         socket_path = rundir / "katex.sock"
 
         # Start the server process
         cmd = cls.build_command(socket=socket_path)
         process = Popen(cmd, stdin=PIPE, stdout=PIPE, cwd=rundir)
 
-        # Wait for the server to come up and create the socket. Is there a
-        # better way to do this?
+        # Wait for the server to come up and create the socket.
         startup_start = time.monotonic()
         while not socket_path.is_socket():
             time.sleep(ONE_MILLISECOND)
@@ -455,12 +402,10 @@ class RenderServer:
 
     @classmethod
     def start_network_socket(cls, rundir, timeout):
-        # Start the server on a random free port and connect to it. This is a
-        # possible race condition because the port might be allocated between
-        # checking its freeness and starting the server but it should be good
-        # enough for now.
+        # Start the server on a random free port and connect to it.
+        # The port may become unavailable in between the check and usage.
         host = "127.0.0.1"
-        port = free_port()
+        port = random_free_port()
 
         # Start the server process
         cmd = cls.build_command(port=port)
@@ -497,43 +442,38 @@ class RenderServer:
         rundir = Path(tempfile.mkdtemp(prefix="pelican_katex"))
 
         if os.name == "posix":
-            process, sock = cls.start_unix_socket(rundir, KATEX_STARTUP_TIMEOUT)
+            process, sock = cls.start_server_process(rundir, STARTUP_TIMEOUT)
         else:
-            # Non-unix systems (i.e. Windows) do not necessarily support unix
-            # domain sockets for IPC, so we fallback to network sockets.
+            # Non-unix systems (i.e. Windows) do not support unix
+            # domain sockets for IPC, so we use network sockets.
             process, sock = cls.start_network_socket(rundir, KATEX_STARTUP_TIMEOUT)
 
-        server = RenderServer(rundir, process, sock)
+        server = KaTeXServer(rundir, process, sock)
 
-        # Clean up after ourselves when pelican is done. This does not work in
-        # live-reload mode if you stop it with ctrl+c because that is not an
-        # orderly exit. However, I don't want to register signal handlers here
-        # and using pelican's signals.finalized has a high performance penalty
-        # because this is triggered after every reload and negates the advantage
-        # of reusing servers across renders.
-        atexit.register(RenderServer.stop, server)
+        # Clean up after ourselves when skphinx is done. I don't want to register
+        # signal handlers here.
+        atexit.register(KaTeXServer.terminate, server)
 
         return server
 
     @classmethod
     def get(cls):
         """Get the current render server or start one"""
-        if cls.RENDER_SERVER is None:
-            cls.RENDER_SERVER = RenderServer.start()
+        if cls.KATEX_SERVER is None:
+            cls.KATEX_SERVER = KaTeXServer.start()
 
-        return cls.RENDER_SERVER
+        return cls.KATEX_SERVER
 
     def __init__(self, rundir, process, sock):
         self.rundir = rundir
         self.process = process
         self.sock = sock
 
-        # Pre-allocate a buffer for the responses. KaTeX renderings usually have
-        # lots of tags so let's start with 100kb.
+        # 100KB should be large enough even for big equations
         self.buffer = bytearray(100 * 1024)
 
-    def stop(self):
-        """Stop the render server and clean up"""
+    def terminate(self):
+        """Terminate the render server and clean up"""
         self.sock.close()
         try:
             self.process.terminate()
@@ -604,16 +544,11 @@ def render_latex(latex, options=None):
         katex_options = katex_options.copy()
         katex_options.update(options)
 
-    # Prepend the preamble
-    preamble = get_preamble()
-    if preamble is not None:
-        latex = preamble + "\n" + latex
-
-    server = RenderServer.get()
+    server = KaTeXServer.get()
     request = {"latex": latex, "katex_options": katex_options}
 
     try:
-        response = server.render(request, KATEX_RENDER_TIMEOUT)
+        response = server.render(request, RENDER_TIMEOUT)
 
         if "html" in response:
             return response["html"]
