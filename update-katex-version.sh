cece2f895f679d10dcf1ec997ba2ea1094803863
Hagen Wierstorf <hwierstorf@audeering.com>
2022-07-08 16:10:58 +0200

    Use katex.min.js

--- update-katex-version.sh
+++ update-katex-version.sh
@@ -11,7 +11,7 @@ echo "Current KaTeX version: ${CURRENT_VERSION}"
 echo "New KaTeX version: ${NEW_VERSION}"
 
 # Download JS file
-curl -s https://cdn.jsdelivr.net/npm/katex@${NEW_VERSION}/dist/katex.js --output sphinxcontrib/katex.js
+curl -s https://cdn.jsdelivr.net/npm/katex@${NEW_VERSION}/dist/katex.min.js --output sphinxcontrib/katex.min.js
 
 # Update Python file
 sed -i "/katex_version = '${CURRENT_VERSION}'/c\\katex_version = '${NEW_VERSION}'" sphinxcontrib/katex.py
