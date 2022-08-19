c336ddd331fb5a3f818abab4107c8b2c2d003169
Hagen Wierstorf <hwierstorf@audeering.com>
2022-08-19 10:12:23 +0200

    Add auto-render.min.js to update script

--- update-katex-version.sh
+++ update-katex-version.sh
@@ -10,8 +10,23 @@ CURRENT_VERSION=${CURRENT_VERSION//\'/}
 echo "Current KaTeX version: ${CURRENT_VERSION}"
 echo "New KaTeX version: ${NEW_VERSION}"
 
-# Download JS file
-curl -s https://cdn.jsdelivr.net/npm/katex@${NEW_VERSION}/dist/katex.min.js --output sphinxcontrib/katex.min.js
+if [ ${NEW_VERSION} -lt ${CURRENT_VERSION} ]; then
+    echo "Something went wrong as current version is newer"
+    exit 1
+fi
+
+if [ ${NEW_VERSION} -eq ${CURRENT_VERSION} ]; then
+    echo "Versions are the same."
+    exit 0
+fi
+
+KATEX_URL="https://cdn.jsdelivr.net/npm/katex@${NEW_VERSION}/dist"
+
+# Download JS files
+echo "Updating katex.min.js"
+curl -s ${KATEX_URL}/katex.min.js --output sphinxcontrib/katex.min.js
+echo "Updating auto-render.min.js"
+curl -s ${KATEX_URL}/auto-render.min.js --output sphinxcontrib/auto-render.min.js
 
 # Update Python file
 sed -i "/katex_version = '${CURRENT_VERSION}'/c\\katex_version = '${NEW_VERSION}'" sphinxcontrib/katex.py
