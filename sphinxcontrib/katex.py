cb55c543ba26a1fd470fcb403b3ede983d798677
Hagen Wierstorf <hwierstorf@audeering.com>
2022-11-25 08:40:13 +0100

    Use KaTeX 0.16.3 (#89)

--- sphinxcontrib/katex.py
+++ sphinxcontrib/katex.py
@@ -35,7 +35,7 @@ from sphinx.util.osutil import copyfile
 
 
 __version__ = '0.9.0'
-katex_version = '0.16.0'
+katex_version = '0.16.3'
 filename_css = 'katex-math.css'
 filename_autorenderer = 'katex_autorenderer.js'
 SRC_DIR = Path(__file__).parent
