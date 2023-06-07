78d6a5e5d8be3a9c6b74faa5d3a9e8e66124abac
w08r <3176756+w08r@users.noreply.github.com>
2023-06-07 11:04:40 +0100

    Convert timeout_error to classmethod (#99)
    * Convert timeout_error to classmethod
    
    timeout_error is called multiple times with the class context;
    arguably could leave it as static method and remove the cls. prefix
    from the other calls but keeping conservative.
    
    Fixes #98
    
    * Rename self to cls for classmethod

--- sphinxcontrib/katex.py
+++ sphinxcontrib/katex.py
@@ -362,8 +362,8 @@ class KaTeXServer:
     # wait for the server to stop in seconds
     STOP_TIMEOUT = 0.1
 
-    @staticmethod
-    def timeout_error(self, timeout):
+    @classmethod
+    def timeout_error(cls, timeout):
         message = STARTUP_TIMEOUT_EXPIRED.format(timeout)
         return KaTeXError(message)
 
